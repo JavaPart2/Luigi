ff442be13b66d2a68d211d35c80e74c78e8a7f24
Marc J <john.doe.gmail.com>
2020-11-19 14:23:08 +0100

    Hfdstk24

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -1,9 +1,8 @@
 package be.vdab.luigi.controllers;
 
-import be.vdab.luigi.domain.Adres;
-import be.vdab.luigi.domain.Persoon;
 import be.vdab.luigi.domain.Pizza;
-import org.apache.catalina.LifecycleState;
+import be.vdab.luigi.exceptions.KoersClientException;
+import be.vdab.luigi.services.EuroService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,6 +22,11 @@ class PizzaController {
             new Pizza(2, "Margherita", BigDecimal.valueOf(5), false),
             new Pizza(3, "Calzone", BigDecimal.valueOf(4), false)
     };
+    private final EuroService euroService;
+
+    PizzaController(EuroService euroService) {
+        this.euroService = euroService;
+    }
 
     @GetMapping
     public ModelAndView pizzas(){
@@ -36,7 +38,15 @@ class PizzaController {
     public ModelAndView pizza(@PathVariable int id){
         ModelAndView modelAndView = new ModelAndView("pizza");
         Arrays.stream(pizzas).filter(pizza -> pizza.getId() == id).findFirst()
-                .ifPresent(pizza -> modelAndView.addObject("pizza", pizza));
+                .ifPresent(pizza -> {
+                    modelAndView.addObject("pizza", pizza);
+                    try {
+                        modelAndView.addObject(
+                                "inDollar", euroService.naarDollar(pizza.getPrijs()));
+                    } catch (KoersClientException ex) {
+
+                    }
+                });
         return modelAndView;
     }
 
---
+++ src/test/java/EuroServiceTest.java
@@ -0,0 +1,31 @@
+import be.vdab.luigi.restclients.KoersClient;
+import be.vdab.luigi.services.DefaultEuroService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EuroServiceTest {
+    @Mock
+    private KoersClient koersClient;
+    private DefaultEuroService euroService;
+
+    @BeforeEach
+    void beforeEach(){
+        euroService = new DefaultEuroService(koersClient);
+    }
+
+    @Test
+    void naarDollar(){
+        when(koersClient.getDollarKoers()).thenReturn(BigDecimal.valueOf(1.5));
+        assertThat(euroService.naarDollar(BigDecimal.valueOf(2)))
+                .isEqualByComparingTo("3");
+    }
+}
