9f9d768bee3ae3ed2580f9a372241e2d4bf1c6ab
Marc J <john.doe.gmail.com>
2020-11-25 11:15:36 +0100

    Forms

--- src/main/java/be/vdab/luigi/controllers/PizzaController.java
+++ src/main/java/be/vdab/luigi/controllers/PizzaController.java
@@ -2,11 +2,13 @@ package be.vdab.luigi.controllers;
 
 import be.vdab.luigi.domain.Pizza;
 import be.vdab.luigi.exceptions.KoersClientException;
+import be.vdab.luigi.forms.VanTotPrijsForm;
 import be.vdab.luigi.services.EuroService;
 import be.vdab.luigi.services.PizzaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,6 +71,21 @@ class PizzaController {
         return modelAndView;
     }
 
+    @GetMapping("vantotprijs/form")
+    public ModelAndView vanTotPrijsForm(){
+        return new ModelAndView("vantotprijs").addObject(new VanTotPrijsForm(BigDecimal.ONE, BigDecimal.TEN));
+
+    }
+
+    @GetMapping("vantotprijs")
+    public ModelAndView vanTotPrijs(VanTotPrijsForm form, Errors errors){
+        ModelAndView modelAndView = new ModelAndView("vantotprijs");
+        if (errors.hasErrors()){
+            return modelAndView;
+        }
+        return modelAndView.addObject("pizzas",
+                pizzaService.findByPrijsBetween(form.getVan(), form.getTot()));
+    }
 /*
     private List<BigDecimal> uniekePrijzen(){
         return Arrays.stream(pizzas).map(pizza -> pizza.getPrijs())
